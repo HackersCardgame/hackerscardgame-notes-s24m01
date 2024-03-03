2f47169cda64e9b8031302705012d54c4ad0ac05
Your Name <you@example.com>
2024-03-04 00:40:41 +0100

    ...

--- tools/inst.sh
+++ tools/inst.sh
@@ -3,4 +3,8 @@ apt-get update
 apt-get upgrade
 apt-get purge firefox-esr
 apt-get install git vim sudo gnome-boxes figlet inkscape gedit mixxx 
-apt-get install libvirt-daemon-system tesseract tesseract-ocr-eng tesseract-ocr-deu
+apt-get install libvirt-daemon-system 
+
+apt-get install tesseract
+
+apt-get install tesseract-ocr-eng tesseract-ocr-deu
