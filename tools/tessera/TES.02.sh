95fda259489b28264f4483c11e20310c228705c3
Your Name <you@example.com>
2024-03-04 01:07:58 +0100

    ...

---
+++ tools/tessera/TES.02.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Auf den Task Scripting schient eine Malware aktiv zu sein, die das kurzzeit-gedächtniss alle 1 sek löscht, teilweile interaktiv zur FTT "das ist das problem was behoben werden muss"
+#
+#Dann weiteres scheint zu sein, dass es irgendwelche klopfgeräsuche in richtgung wand hinter meinem sessel zu geben
+
+#dann scheint es irgend eine "instant verkrustungs-funktion" meiner nasen innenseite zu geben die grad aktiviert wurde
+
+#dann das geräusch scheint irgend ein holrauum zu sein wo mutmasslich die geistteskranke Tremp jetzt schienbar von jemandem einen Terror-Afurrga bekommen hat
+
+#dann scheint ICD10 F44.x funktion aktiv zu sein, für jedes teilscript wird ein anderer Teil des gehirns gesperrt, bzw, nur ein tel des geheirns freigegben
+#
+#hiesse das sind neurologie-terroristen sowohl asuf meinem neuralink als auch auf meinem offline computer
+
+echo TES.sh srcFILE targetFILE subDir
+
+subDir="$(echo $3|cut -c3-)"
+echo SUBDIR: $subDir
+
+mkdir -p "$(dirname "${2}$subDir}")"
+
+echo EE tesseract $1 $2$subDir
+tesseract $1 $2$subDir
+
