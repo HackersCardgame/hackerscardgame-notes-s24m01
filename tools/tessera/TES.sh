11a4c4a29dcd8a50851c111fbba2cdd025149672
Your Name <you@example.com>
2024-03-04 01:03:18 +0100

    ...

---
+++ tools/tessera/TES.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Auf den Task Scripting schient eine Malware aktiv zu sein, die das kurzzeit-gedächtniss alle 1 sek löscht, teilweile interaktiv zur FTT "das ist das problem was behoben werden muss"
+#
+#Dann weiteres scheint zu sein, dass es irgendwelche klopfgeräsuche in richtgung wand hinter meinem sessel zu geben
+
+#dann scheint es irgend eine "instant verkrustungs-funktion" meiner nasen innenseite zu geben die grad aktiviert wurde
+
+#dann das geräusch scheint irgend ein holrauum zu sein wo mutmasslich die geistteskranke Tremp jetzt schienbar von jemandem einen Terror-Afurrga bekommen hat
+
+
+echo TES.sh srcFILE targetFILE subDir
+
+subDir="$(echo $3|cut -c3-)"
+echo SUBDIR: $subDir
+
+mkdir -p "$(dirname "${2}$subDir}")"
+
+echo EE tesseract $1 $2$subDir
+#echo tesseract $1 $1
+
